262987173ef7c574703ea0cd365b01b1640ca305
Chris Kelley <ckemail2@gmail.com>
2019-08-15 12:43:48 -0400

    add enemy movement

--- js/app.js
+++ js/app.js
@@ -5,10 +5,14 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.x = 0;
-    this.y = 0;
+    this.xMove = 101; // 101 is width of each column, distance to next column
+    this.yMove = 83; // 83 is height of each row, distance to next row
+    this.xStart = -202; // start two xMoves off of the screen
+    this.yStart = (this.yMove * 1) - 25; // Start on 2nd row, top row of roadway
+    this.x = this.xStart;
+    this.y = this.yStart;
     this.sprite = 'images/enemy-bug.png';
-    // Set the enemy's speed
+    this.speed = 0; // Set the enemy's speed
 };
 
 // Update the enemy's position, required method for game
@@ -17,6 +21,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.xMove * dt;
+    if (this.x > 505) { // if enemy is off the screen, reset to start position
+        this.reset();
+    }
+
+};
+
+Enemy.prototype.reset = function() {
+    this.x = this.xStart; // restet to two xMoves off of the screen
 };
 
 // Draw the enemy on the screen, required method for game
@@ -27,7 +40,6 @@ Enemy.prototype.render = function() {
 
 Enemy.prototype.handleCollision = function() {
     // handle collision with the player
-
 }
 
 
@@ -86,10 +98,11 @@ class Hero {
 
 
 // Now instantiate your objects.
-
+const enemy1 = new Enemy();
 
 // Place all enemy objects in an array called allEnemies
-
+const allEnemies = []
+allEnemies.push(enemy1);
 
 // Place the player object in a variable called player
 const player = new Hero();
--- js/engine.js
+++ js/engine.js
@@ -90,12 +90,10 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-      /*
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
         player.update();
-      */
     }
 
     /* This function initially draws the "game level", it will then call
@@ -151,15 +149,11 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-
-        /*
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-        */
 
         player.render();
-      
     }
 
     /* This function does nothing but it could have been a good place to
